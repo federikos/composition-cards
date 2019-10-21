2c5546557a9aa8e40e72d4a8360f85a50e8ba909
federikos <federikos@bk.ru>
2019-10-21 19:08:39 +0300

    replace classes to classNames

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
   return (
     <div className="App">
       <Card img='https://via.placeholder.com/285x180?text=Image cap'>
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
       </Card>
       <Card>
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
       </Card>
     </div>
   );
